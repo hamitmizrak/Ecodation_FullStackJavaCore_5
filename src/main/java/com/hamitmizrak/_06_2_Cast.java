574b86f56364d6928a5e20660e89cf68daf185d4
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-07 10:45:55 +0300

    cast

--- src/main/java/com/hamitmizrak/_06_2_Cast.java
+++ src/main/java/com/hamitmizrak/_06_2_Cast.java
@@ -3,8 +3,13 @@ package com.hamitmizrak;
 public class _06_2_Cast {
 
     // User
-   static void main() {
-       // Scanner
+    static void main() {
+        String number1="44";
+        int number2=16;
+        System.out.println(number1+number2);
 
-   }
+        // Cast
+        int number3=Integer.valueOf(number1);
+        System.out.println(number2+number3);
+    }
 }
