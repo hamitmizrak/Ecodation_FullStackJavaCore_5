6188751f7fd8ede8ed2f247e16836865f7e60900
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-07 10:46:54 +0300

    cast

--- src/main/java/com/hamitmizrak/_06_2_Cast.java
+++ src/main/java/com/hamitmizrak/_06_2_Cast.java
@@ -4,12 +4,12 @@ public class _06_2_Cast {
 
     // User
     static void main() {
-        String number1="44";
-        int number2=16;
-        System.out.println(number1+number2);
+        String number1 = "44";
+        int number2 = 16;
+        System.out.println(number1 + number2);
 
         // Cast
-        int number3=Integer.valueOf(number1);
-        System.out.println(number2+number3);
-    }
-}
+        int number3 = Integer.valueOf(number1);
+        System.out.println(number2 + number3);
+    } //end psvm
+} //end _06_2_Cast
