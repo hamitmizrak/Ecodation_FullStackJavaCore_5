e9488cf684c3d8732c1836af64352e2e00328dec
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-07 10:03:44 +0300

    datatypes

---
+++ src/main/java/com/hamitmizrak/_02_1_DataTypesPrimitive.java
@@ -0,0 +1,24 @@
+package com.hamitmizrak;
+
+public class _02_1_DataTypesPrimitive {
+
+    // PSVM
+    static void main() {
+
+        // Primitive Type
+        //byte $_primitiveData =-128;
+        byte primitiveData1 =-128;
+        short primitiveData2 =140;
+        int primitiveData3 =1234567890;
+        long primitiveData4 =1234567890123L;
+
+        float primitiveData5=14.53F;
+        double primitiveData6=10.4110453;
+
+        char primitiveData7='&';
+        boolean primitiveData8=true;
+
+        // Örnek
+        // int data=null;  //null değer alınmaz
+    }
+}
---
+++ src/main/java/com/hamitmizrak/_02_2_DataTypesWrapper.java
@@ -0,0 +1,24 @@
+package com.hamitmizrak;
+
+public class _02_2_DataTypesWrapper {
+
+    // PSVM
+    static void main() {
+
+        // Wrapper Type
+        //byte $_wrapperData =-128;
+        Byte wrapperData1 = -128;
+        Short wrapperData2 = 140;
+        Integer wrapperData3 = 1234567890;
+        Long wrapperData4 = 1234567890123L;
+
+        Float wrapperData5 = 14.53F;
+        Double wrapperData6 = 10.4110453;
+
+        Character wrapperData7 = '&';
+        Boolean wrapperData8 = true;
+
+        // Örnek
+        Integer data = null;  //null değer alınır
+    }
+}
