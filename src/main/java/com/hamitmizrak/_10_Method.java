a833ebd3597a4632311789c081f3481c042e6f78
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-07 13:07:54 +0300

    Method

--- src/main/java/com/hamitmizrak/_10_Method.java
+++ src/main/java/com/hamitmizrak/_10_Method.java
@@ -2,9 +2,40 @@ package com.hamitmizrak;
 
 public class _10_Method {
 
-    // User
-   static void main() {
-       // Scanner
+    // 1-) parametresiz-returnsuz
+    public static void parametresizReturnsuz() {
+        System.out.println(" parametresiz-returnsuz");
+    }
 
-   }
+    // 2-) parametreli-returnsuz
+    public static void parametreliReturnsuz(String name) {
+        System.out.println(" parametreli-returnsuz " + name);
+    }
+
+    // 3-) parametresiz-returnlu
+    public static String parametresizReturnlu() {
+        return " parametresiz-returnlu";
+    }
+
+    // 4-) parametreli-returnlu
+    public static String parametreliReturnlu(String name, String surname) {
+        return " parametreli-returnlu "+name+surname;
+    }
+
+
+    static void main() {
+        parametresizReturnsuz();
+        parametreliReturnsuz("Hamit");
+        String result1 = parametresizReturnlu();
+        System.out.println(result1);
+
+        String result2 = parametreliReturnlu("Hamit","Mızrak");
+        System.out.println(result2);
+    }
+
 }
+
+// static
+// break
+// return
+// continue
--- src/main/java/com/hamitmizrak/_12_1_AllCurciliumExamples.java
+++ src/main/java/com/hamitmizrak/_12_1_AllCurciliumExamples.java
@@ -2,9 +2,8 @@ package com.hamitmizrak;
 
 public class _12_1_AllCurciliumExamples {
 
-    // User
-   static void main() {
-       // Scanner
-
-   }
+// static
+// break
+// return
+// continue
 }
