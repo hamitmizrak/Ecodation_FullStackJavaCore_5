bc766c88a45e56bef3e63c45a28fe85692bfa80c
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-07 12:49:59 +0300

    String

--- src/main/java/com/hamitmizrak/_09_1_String.java
+++ src/main/java/com/hamitmizrak/_09_1_String.java
@@ -4,6 +4,25 @@ public class _09_1_String {
 
     // String
    static void main() {
-       // Scanner
+       String value=" javase javame JAVAee javase";
+       System.out.println(value);
+       System.out.println(value.length());
+       System.out.println(value.trim());
+       System.out.println(value.trim().length());
+       System.out.println(value.startsWith("javase"));
+       System.out.println(value.endsWith("javase"));
+
+       value =value.trim();
+       System.out.println(value.substring(1));
+       System.out.println(value.substring(1,4)); // 1<=SAYI<=4-1
+
+       System.out.println(value.toLowerCase());
+       System.out.println(value.toUpperCase());
+       System.out.println(value.indexOf("javase"));
+       System.out.println(value.lastIndexOf("javase"));
+       System.out.println(value.isEmpty());
+       System.out.println(value.charAt(0));
+       System.out.println(value.contains("javase"));
+       System.out.println(value.hashCode());
    }
 }
--- src/main/java/com/hamitmizrak/_09_2_StringBuilder.java
+++ src/main/java/com/hamitmizrak/_09_2_StringBuilder.java
@@ -3,8 +3,22 @@ package com.hamitmizrak;
 public class _09_2_StringBuilder {
 
     // User
-   static void main() {
-       // Scanner
+    static void main() {
+        String value1 = "Malatya", value2 = "Bingöl", value3 = "Sivas";
 
-   }
+        // 1.YOL
+        String result1 = value1 + value2 + value3;
+        System.out.println(result1);
+
+        // 2.YOL
+        String result2 = value1.concat(value2).concat(value3);
+        System.out.println(result2);
+
+        //3.YOL
+        StringBuilder stringBuilder= new StringBuilder();
+        stringBuilder.append(value1).append(value2).append(value3);
+        String result3= stringBuilder.toString();
+        System.out.println(result3);
+
+    }
 }
