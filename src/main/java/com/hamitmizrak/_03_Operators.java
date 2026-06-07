53084c787a90101b91a12c2a788196352b9650de
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-07 10:03:53 +0300

    operators

---
+++ src/main/java/com/hamitmizrak/_03_Operators.java
@@ -0,0 +1,20 @@
+package com.hamitmizrak;
+
+public class _03_Operators {
+
+    // Operator
+   static void main() {
+
+       // Aritmetik Operators
+       System.out.println(11+3);
+       System.out.println(11-3);
+       System.out.println(11*3);
+       System.out.println(11/3);
+       System.out.println(11%3);
+
+       // && VE
+       // || VEYA
+       // !  DEĞİL
+
+   }
+}
