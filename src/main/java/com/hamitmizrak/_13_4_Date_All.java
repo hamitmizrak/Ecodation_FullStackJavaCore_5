9eb72fe89ffed237a68e489ce4d6c9d97a54496a
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-13 11:37:36 +0300

    date calendar localdatetime

--- src/main/java/com/hamitmizrak/_13_4_Date_All.java
+++ src/main/java/com/hamitmizrak/_13_4_Date_All.java
@@ -1,11 +1,13 @@
 package com.hamitmizrak;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 // Date 1900 (+-)
 public class _13_4_Date_All {
 
-    static void main() {
+    public static void allData(){
         Date now= new Date();
         System.out.println("Şimdiki zaman: "+now);
         // 1 OCAK 1970   - 13 Haziran 2026 milisaiye (1000ms=1s)
@@ -18,7 +20,21 @@ public class _13_4_Date_All {
         // SET
         now.setHours(5);
         System.out.println("Şimdiki saat: "+now.getHours());
+    }
 
+    public static void dataFormat(){
+        Date date = new Date();
+        System.out.println(date);
+
+        Locale locale = new Locale("tr","TR");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MMMM/yyyy hh:mm:ss",locale);
+        String formattedDate= simpleDateFormat.format(date);
+        System.out.println(formattedDate);
+    }
+
+    static void main() {
+        //allData();
+        dataFormat();
 
 
     }
