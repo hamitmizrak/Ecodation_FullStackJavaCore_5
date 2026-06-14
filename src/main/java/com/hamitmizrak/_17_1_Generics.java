7ff382e899a61f0dd7b6aa478ad63f0f235dd20a
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-14 11:51:53 +0300

    Generics

--- src/main/java/com/hamitmizrak/_17_1_Generics.java
+++ src/main/java/com/hamitmizrak/_17_1_Generics.java
@@ -1,4 +1,55 @@
 package com.hamitmizrak;
 
-public class _17_1_Generics {
+public class _17_1_Generics <T> {
+    private String name;
+    private T number;
+
+    // Parametresiz Constructor
+    // Parametreli Constructor
+    // ToString()
+    @Override
+    public String toString() {
+        return "_17_1_Generics{" +
+                "name='" + name + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
+
+    // Method
+    public void noGenericMethod(String data){
+        System.out.println(data);
+    }
+
+    public <K> void genericMethod  (K data){
+        System.out.println(data);
+    }
+
+
+    // Getter/Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public T getNumber() {
+        return number;
+    }
+
+    public void setNumber(T number) {
+        this.number = number;
+    }
+
+    static void main() {
+        _17_1_Generics ornek= new _17_1_Generics();
+        ornek.setName("Hamit");
+        ornek.setNumber(true);
+        System.out.println(ornek);
+
+        ornek.noGenericMethod("Generic olamayan method");
+        ornek.genericMethod(454545.5252);
+
+    }
 }
