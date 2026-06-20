2d0c742413ab91225beffc603bdc54cbd3e8cbb0
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-20 10:37:54 +0300

    inheritance

---
+++ src/main/java/com/hamitmizrak/oop/interfacex/ICrud.java
@@ -0,0 +1,10 @@
+package com.hamitmizrak.oop.interfacex;
+
+public interface ICrud {
+
+    // Create
+    public void create();
+    public void list();
+    public void update();
+    public void delete();
+}
