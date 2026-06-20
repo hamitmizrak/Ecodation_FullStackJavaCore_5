f13b6a49b3ca5f23761bcdab644701b06dd31510
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-20 10:51:24 +0300

    inheritance abstract

--- src/main/java/com/hamitmizrak/oop/interfacex/Student.java
+++ src/main/java/com/hamitmizrak/oop/interfacex/Student.java
@@ -1,4 +1,53 @@
 package com.hamitmizrak.oop.interfacex;
 
-public class Student {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+// Lombok
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Student extends Person implements ICrud, Serializable {
+
+    // Field
+    private String number;
+
+    // Constructor
+    public Student(Long id, String name, String surname, String number) {
+        super(id, name, surname); //üst class'dan gelen özellikler
+        this.number = number;
+    }
+
+    // abstract Gövdesiz Methodun
+    @Override
+    public void noBodyMethod() {
+
+    }
+
+    // Crud
+    @Override
+    public void create() {
+        System.out.println("Student create");
+    }
+
+    @Override
+    public void list() {
+        System.out.println("Student list");
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Student update");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("Student delete");
+    }
 }
--- src/main/java/com/hamitmizrak/oop/interfacex/Teacher.java
+++ src/main/java/com/hamitmizrak/oop/interfacex/Teacher.java
@@ -1,4 +1,55 @@
 package com.hamitmizrak.oop.interfacex;
 
-public class Teacher {
-}
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+// Lombok
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Teacher  extends Person implements ICrud, Serializable {
+
+    // Field
+    private Double price;
+
+    // Constructor
+    public Teacher(Long id, String name, String surname, Double price) {
+        super(id, name, surname);
+        this.price = price;
+    }
+
+
+    // abstract Gövdesiz Methodun
+    @Override
+    public void noBodyMethod() {
+
+    }
+
+    // Interface
+    @Override
+    public void create() {
+        System.out.println("Teacher create");
+    }
+
+    @Override
+    public void list() {
+        System.out.println("Teacher list");
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Teacher update");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("Teacher delete");
+    }
+
+} // end Teacher
